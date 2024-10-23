65ca24f302ed6deafdc6708c79be3ce75eb24476
Gabriel Cardoso <gabrielcardoso.s1998@gmail.com>
2024-10-23 19:58:48 -0300

    Exercicio de interseção

--- Projetos/24-10-2024/atletismobasquete.py
+++ Projetos/24-10-2024/atletismobasquete.py
@@ -19,60 +19,63 @@ def Verificar():
     verificar = set(atletismo) and set(basquete)
     return list(verificar)
 
-while True:
-    options = [
-        '[1] Adicionar estudante',
-        '[2] Ver estudantes em ambas as atividades',
-        '[3] Sair',
-    ]
-    for option in options:
-        Digitar(option + '\n')
-    
-    try:
-        Digitar('Escolha uma opção: ')
-        escolha = int(input('> '))
+def Menu_Principal():
+    while True:
+        options = [
+            '[1] Adicionar estudante',
+            '[2] Ver estudantes em ambas as atividades',
+            '[3] Sair',
+        ]
+        for option in options:
+            Digitar(option + '\n')
         
-        if escolha == 1:
-            while True:
-                options2 = [
-                    '[1] Adicionar ao atletismo',
-                    '[2] Adicionar ao basquete'
-                ]
-                for option in options2:
-                    Digitar(option + '\n')
-                
-                Digitar('Escolha uma opção: ')
-                escolha_esporte = int(input('> '))
-                
-                if escolha_esporte in [1, 2]:
-                    Digitar('Digite o nome do aluno: ')
-                    nome_aluno = input('> ')
-                    Add_Estudante(escolha_esporte, nome_aluno)
-                    Digitar(f'Aluno {nome_aluno} adicionado com sucesso!\n')
-                else:
-                    Digitar('Digite uma opção válida!\n')
-                
-                Digitar('Deseja adicionar outro estudante? (s/n): ')
-                continuar = input('> ').strip().lower()
-                if continuar != 's':
-                    break
+        try:
+            Digitar('Escolha uma opção: ')
+            escolha = int(input('> '))
+            
+            if escolha == 1:
+                while True:
+                    options2 = [
+                        '[1] Adicionar ao atletismo',
+                        '[2] Adicionar ao basquete'
+                    ]
+                    for option in options2:
+                        Digitar(option + '\n')
+                    
+                    Digitar('Escolha uma opção: ')
+                    escolha_esporte = int(input('> '))
+                    
+                    if escolha_esporte in [1, 2]:
+                        Digitar('Digite o nome do aluno: ')
+                        nome_aluno = input('> ')
+                        Add_Estudante(escolha_esporte, nome_aluno)
+                        Digitar(f'Aluno {nome_aluno} adicionado com sucesso!\n')
+                    else:
+                        Digitar('Digite uma opção válida!\n')
+                    
+                    Digitar('Deseja adicionar outro estudante? (s/n): ')
+                    continuar = input('> ').strip().lower()
+                    if continuar != 's':
+                        break
 
-        elif escolha == 2:
-            estudantes_simultaneos = Verificar()
-            if estudantes_simultaneos:
-                Digitar('Estudantes que praticam ambas as atividades:\n')
-                for estudante in estudantes_simultaneos:
-                    Digitar(f'- {estudante}\n')
+            elif escolha == 2:
+                estudantes_simultaneos = Verificar()
+                if estudantes_simultaneos:
+                    Digitar('Estudantes que praticam ambas as atividades:\n')
+                    for estudante in estudantes_simultaneos:
+                        Digitar(f'- {estudante}\n')
+                else:
+                    Digitar('Nenhum estudante está praticando ambas as atividades.\n')
+            
+            elif escolha == 3:
+                Digitar('Finalizando...\n')
+                break
             else:
-                Digitar('Nenhum estudante está praticando ambas as atividades.\n')
+                Digitar('Digite uma opção válida!\n')
+                time.sleep(1)
         
-        elif escolha == 3:
-            Digitar('Finalizando...\n')
-            break
-        else:
-            Digitar('Digite uma opção válida!\n')
+        except ValueError:
+            Digitar('Escolha uma opção válida!\n')
             time.sleep(1)
-    
-    except ValueError:
-        Digitar('Escolha uma opção válida!\n')
-        time.sleep(1)
+
+Menu_Principal()
---
+++ Projetos/24-10-2024/calculointersecao.py
@@ -0,0 +1,7 @@
+
+conjunto1 = {1, 2, 3, 4}
+conjunto2 = {3, 4, 5, 6}
+
+interseção = conjunto1.intersection(conjunto2)
+
+print("A interseção entre os conjuntos é:", interseção)
