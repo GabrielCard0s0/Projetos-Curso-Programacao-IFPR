461aa3ecade06156496b1498e92181a403975a77
Gabriel Cardoso <gabrielcardoso.s1998@gmail.com>
2024-10-22 21:50:24 -0300

    Update calculo.py

--- Projetos/22-10-2024/Tuplas/calculo.py
+++ Projetos/22-10-2024/Tuplas/calculo.py
@@ -3,7 +3,7 @@ from math import sqrt
 
 def Calculo(p1, p2):
     return math.sqrt((p1[0] - p2[0])** 2 + 
-                     (p2[1] - p2[1])** 2 + 
+                     (p1[1] - p2[1])** 2 + 
                      (p1[2] - p2[2])** 2)
 
 def main():
